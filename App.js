b06a0a0cdf8c2057f9eeb38578d3612e158d66ca
Mason Brothers <brothers.mason@gmail.com>
2017-08-26 11:58:35 -0600

    Not sure if this builds/runs... Cannot test it on my computer...

--- App.js
+++ App.js
@@ -2,14 +2,108 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  create() {
+    super.create();
+  }
+
   render() {
+    load();
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <input type="text" id="userInput"></input>
+        <input type="button" onClick={this.save} value="Save"></input>
+        <input type="button" onClick={this.load} value="Load"></input>
+
       </View>
     );
+  }
+
+  userData = [
+    {
+      emoji: "\u0123\u1233",
+      videoPath: "",
+      text: ""
+    }
+  ]
+
+  DEBUG_MESSAGE = "\nIf you see this message in production, please contact the development team.";
+
+  /**
+   * This Function takes a string input, and returns the emoji if appropriate.
+   * @param {*The string that the user inputs. We will only grab the first unicode character of the string.} userString 
+   */
+  emojiStringToEncodedEmoji(emojiString) {
+    if (typeof(emojiString) !== "string") {
+      throw "Sorry! Please input a string of one character!";
+    } else if (emojiString.length == 0) {
+      throw "Sorry! Please enter a single character or emoji! You entered nothing!";
+    } else if (emojiString.length > 2) {
+      throw "Sorry! Please enter a single character or emoji! You entered too many characters.";
+    } else if (emojiString.length == 2) {
+      // This is either an emoji (2 characters), or 2 ascii characters. Let us determine.
+      if (emojiString.charCodeAt(0) == emojiString.codePointAt(0)) {
+        // If the first character is the same as 
+        // This means that there are two characters
+        throw "Sorry! Please enter a single character or emoji! You entered 2 characters.";
+      }
+    }
+    return emojiString.codePointAt(0);
+  }
+
+  /**
+   * This converts a number representing an encoded emoji into a string!
+   * @param {*This is a number, that encodes for the emoji or character that will be shown.} encodedEmoji 
+   */
+  encodedEmojiToEmojiString(encodedEmoji) {
+    if (typeof(encodedEmoji) !== "number") {
+      throw "Sorry! Please ensure that the encodedEmoji is stored as a number." + DEBUG_MESSAGE;
+    }
+    if (encodedEmoji < 0 || encodedEmoji > 0x10FFFF) {
+      throw "Sorry! Please ensure that the encodedEmoji is between 0 and 0x10FFF0." + DEBUG_MESSAGE;
+    }
+    return String.fromCodePoint(encodedEmoji);
+  }
+
+  appendData(emoji, videoPath, text) {
+    data.push({
+      emoji: emoji,
+      videoPath: videoPath,
+      text: text
+    })
+    save();
+  }
+
+  /**
+   * This saves the values stored in the data 
+   */
+  save() {
+    const dataStructure = userData;
+
+    const dataInJSON = JSON.stringify(dataStructure);
+    try {
+      await AsyncStorage.setItem('@saveData', dataInJSON);
+    } catch (error) {
+      Alert.alert("Cannot Save Data");
+    }
+  }
+
+  /**
+   * This loads the data 
+   */
+  load() {
+    try {
+      const dataInJSON = await AsyncStorage.getItem('@saveData');
+      if (dataInJSON !== null){
+        const dataStructure = JSON.parse(dataInJSON);
+        // Update the data
+        userData = dataStructure
+      }
+    } catch (error) {
+      Alert.alert("Cannot Load Data");
+    }
   }
 }
 
