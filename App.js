00050b10ff67a03b2a09b7d7ff89c60bc69a47d0
Mason Brothers <brothers.mason@gmail.com>
2017-08-26 14:41:50 -0600

    ES6 rules!

--- App.js
+++ App.js
@@ -35,19 +35,19 @@ export default class App extends React.Component {
    * This Function takes a string input, and returns the emoji if appropriate.
    * @param {*The string that the user inputs. We will only grab the first unicode character of the string.} userString 
    */
-  emojiStringToEncodedEmoji(emojiString) {
+  emojiStringToEncodedEmoji = (emojiString) => {
     if (typeof(emojiString) !== "string") {
-      throw "Sorry! Please input a string of one character!";
-    } else if (emojiString.length == 0) {
-      throw "Sorry! Please enter a single character or emoji! You entered nothing!";
+      throw Aut2TalkError("Sorry! Please input a string of one character!");
+    } else if (emojiString.length === 0) {
+      throw Aut2TalkError("Sorry! Please enter a single character or emoji! You entered nothing!");
     } else if (emojiString.length > 2) {
-      throw "Sorry! Please enter a single character or emoji! You entered too many characters.";
-    } else if (emojiString.length == 2) {
+      throw Aut2TalkError("Sorry! Please enter a single character or emoji! You entered too many characters.");
+    } else if (emojiString.length === 2) {
       // This is either an emoji (2 characters), or 2 ascii characters. Let us determine.
-      if (emojiString.charCodeAt(0) == emojiString.codePointAt(0)) {
+      if (emojiString.charCodeAt(0) === emojiString.codePointAt(0)) {
         // If the first character is the same as 
         // This means that there are two characters
-        throw "Sorry! Please enter a single character or emoji! You entered 2 characters.";
+        throw Aut2TalkError("Sorry! Please enter a single character or emoji! You entered 2 characters.");
       }
     }
     return emojiString.codePointAt(0);
@@ -57,53 +57,56 @@ export default class App extends React.Component {
    * This converts a number representing an encoded emoji into a string!
    * @param {*This is a number, that encodes for the emoji or character that will be shown.} encodedEmoji 
    */
-  encodedEmojiToEmojiString(encodedEmoji) {
+  encodedEmojiToEmojiString = (encodedEmoji) => {
     if (typeof(encodedEmoji) !== "number") {
-      throw "Sorry! Please ensure that the encodedEmoji is stored as a number." + DEBUG_MESSAGE;
+      throw Aut2TalkError("Sorry! Please ensure that the encodedEmoji is stored as a number." + this.DEBUG_MESSAGE);
     }
     if (encodedEmoji < 0 || encodedEmoji > 0x10FFFF) {
-      throw "Sorry! Please ensure that the encodedEmoji is between 0 and 0x10FFF0." + DEBUG_MESSAGE;
+      throw Aut2TalkError("Sorry! Please ensure that the encodedEmoji is between 0 and 0x10FFF0." + this.DEBUG_MESSAGE);
     }
     return String.fromCodePoint(encodedEmoji);
   }
 
-  appendData(emoji, videoPath, text) {
-    data.push({
+  appendData = (emoji, videoPath, text) => {
+    this.userData.push({
       emoji: emoji,
       videoPath: videoPath,
       text: text
     })
-    save();
+    this.save();
   }
 
   /**
    * This saves the values stored in the data 
    */
-  save() {
-    const dataStructure = userData;
-
-    const dataInJSON = JSON.stringify(dataStructure);
-    try {
-      await AsyncStorage.setItem('@saveData', dataInJSON);
-    } catch (error) {
-      Alert.alert("Cannot Save Data");
-    }
+  save = async () => {
+      const dataStructure = this.userData;
+      const dataInJSON = JSON.stringify(dataStructure);
+      try {
+        await AsyncStorage.setItem('@saveData', dataInJSON);
+      } catch (error) {
+        Alert.alert("Cannot Save Data");
+      }
+      return true;
   }
 
   /**
    * This loads the data 
    */
-  load() {
+  load = async () => {
     try {
       const dataInJSON = await AsyncStorage.getItem('@saveData');
       if (dataInJSON !== null){
         const dataStructure = JSON.parse(dataInJSON);
         // Update the data
-        userData = dataStructure
+        this.userData = dataStructure
+        return dataStructure
       }
+      return null;
     } catch (error) {
       Alert.alert("Cannot Load Data");
     }
+    return null;
   }
 }
 
